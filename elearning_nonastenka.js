797e68e4ffb6ca08ebb2833ada3be5097ea6bc52
Ondřej Mekina <145680072+ondrejmekina@users.noreply.github.com>
2024-02-25 12:17:12 +0100

    Who needs the nástěnka anyways

---
+++ elearning_nonastenka.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Redirect to courses in elearning
+// @namespace   https://elearning.tul.cz/
+// @include     https://elearning.tul.cz/my/
+// @version     1
+// ==/UserScript==
+
+
+window.location.href = "https://elearning.tul.cz/my/courses.php";
